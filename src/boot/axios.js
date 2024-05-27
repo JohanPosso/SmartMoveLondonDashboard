41c086d94e33774489405e493c018d655d8b2503
JohanPosso <johan-posito2013@hotmail.com>
2024-05-27 20:49:27 +0100

    feat: Se agrego archivos para PWA

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: process.env.BACKEND_URI,
-  baseURL: "https://smartmovelondondashboard-task.up.railway.app",
+  baseURL: process.env.BACKEND_URI,
+  // baseURL: "https://smartmovelondondashboard-task.up.railway.app",
   withCredentials: false,
 });
 export default api;

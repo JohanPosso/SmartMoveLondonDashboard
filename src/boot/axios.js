efb0a331bf9a8fcad61290042e9841c429204641
JohanPosso <johan-posito2013@hotmail.com>
2024-05-27 19:32:03 +0100

    fix: cambio de variable para axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.BACKEND_URI,
-  // withCredentials: false,
+  //   withCredentials: true,
+  baseURL: "https://smartmovelondondashboard-task.up.railway.app",
+  withCredentials: false,
 });
 export default api;

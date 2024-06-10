a5fe3949910a05805b3863eceb43160e8cc51171
JohanPosso <johan-posito2013@hotmail.com>
2024-06-10 23:51:32 +0100

    feat: Se agrego funcionalidad para modificar usuarios

--- src/views/components/AuthorsTable.vue
+++ src/views/components/AuthorsTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div v-if="!isEditing" class="card">
     <div class="card-header pb-0">
       <h6>Employees</h6>
     </div>
@@ -28,6 +28,11 @@
                 class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
               >
                 Country
+              </th>
+              <th
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                Option
               </th>
             </tr>
           </thead>
@@ -74,17 +79,151 @@
                   item.pais
                 }}</span>
               </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  ><button @click="editUser(item.id_empleado)" class="btn">
+                    Edit
+                  </button></span
+                >
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
   </div>
+
+  <div v-else class="edit-temple">
+    <div class="py-4 container-fluid">
+      <div class="row" style="display: ruby">
+        <div class="col-md-9">
+          <div class="card">
+            <div class="card-header pb-0">
+              <div class="d-flex align-items-center">
+                <argon-button
+                  @click="submitForm"
+                  color="success"
+                  size="sm"
+                  class="ms-auto"
+                  >Submit</argon-button
+                >
+              </div>
+            </div>
+            <div class="card-body">
+              <p class="text-uppercase text-sm">EDIT USER INFORMATION</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >Gender</label
+                  >
+                  <select
+                    class="form-select"
+                    v-model="genero"
+                    id="floatingSelect"
+                  >
+                    <option value="1">Male</option>
+                    <option value="2">Female</option>
+                    <option value="3">Other</option>
+                  </select>
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >Name *</label
+                  >
+                  <input
+                    id="nombres"
+                    v-model="nombres"
+                    class="form-control"
+                    type="text"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >Surname *</label
+                  >
+                  <argon-input
+                    id="primer_apellido"
+                    v-model="primer_apellido"
+                    type="text"
+                  />
+                </div>
+
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >Second surname</label
+                  >
+                  <argon-input v-model="segundo_apellido" type="text" />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >Email *</label
+                  >
+                  <argon-input v-model="email" type="email" />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >Phone *</label
+                  >
+                  <argon-input v-model="telefono" type="phone" />
+                </div>
+
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >Date of Birth</label
+                  >
+                  <argon-input v-model="fecha_nacimiento" type="date" />
+                </div>
+              </div>
+              <hr class="horizontal dark" />
+              <p class="text-uppercase text-sm">Residence Information</p>
+              <div class="row">
+                <div class="col-md-12">
+                  <label for="example-text-input" class="form-control-label"
+                    >Address</label
+                  >
+                  <argon-input v-model="direccion" type="text" />
+                </div>
+                <div class="col-md-4">
+                  <label for="example-text-input" class="form-control-label"
+                    >City</label
+                  >
+                  <argon-input v-model="ciudad" type="text" />
+                </div>
+                <div class="col-md-4">
+                  <label for="example-text-input" class="form-control-label"
+                    >Country</label
+                  >
+                  <argon-input v-model="pais" type="text" />
+                </div>
+                <div class="col-md-4">
+                  <label for="example-text-input" class="form-control-label"
+                    >Postcode</label
+                  >
+                  <argon-input v-model="postcode" type="text" />
+                </div>
+              </div>
+              <hr class="horizontal dark" />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <profile-card />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onBeforeMount, onMounted, onBeforeUnmount } from "vue";
+import { useStore } from "vuex";
 import api from "/src/boot/axios";
+import setNavPills from "@/assets/js/nav-pills.js";
+import setTooltip from "@/assets/js/tooltip.js";
+import ArgonInput from "@/components/ArgonInput.vue";
+import ArgonButton from "@/components/ArgonButton.vue";
+const isEditing = ref(false);
+const idEmpleado = ref([]);
 
 const users = ref([]);
 onMounted(async () => {
@@ -116,10 +255,70 @@ const statusChange = async (id) => {
     console.error("Error al cargar los usuarios:", error);
   }
 };
+
+const editUser = async (id_empleado) => {
+  isEditing.value = true;
+  idEmpleado.value.push(id_empleado);
+};
+
+const body = document.getElementsByTagName("body")[0];
+const store = useStore();
+const genero = ref("");
+const nombres = ref("");
+const primer_apellido = ref();
+const segundo_apellido = ref();
+const email = ref();
+const telefono = ref();
+const fecha_nacimiento = ref();
+const direccion = ref();
+const pais = ref();
+const postcode = ref();
+
+onMounted(() => {
+  store.state.isAbsolute = true;
+  setNavPills();
+  setTooltip();
+});
+onBeforeMount(() => {
+  store.state.imageLayout = "profile-overview";
+  store.state.showNavbar = false;
+  store.state.hideConfigButton = true;
+  body.classList.add("profile-overview");
+});
+onBeforeUnmount(() => {
+  store.state.isAbsolute = false;
+  store.state.imageLayout = "default";
+  store.state.showNavbar = true;
+  store.state.hideConfigButton = false;
+  body.classList.remove("profile-overview");
+});
+const submitForm = async () => {
+  const id_empleado = idEmpleado.value.map((e) => e);
+  const response = await api.put(`/edituser/${id_empleado[0]}`, {
+    id_empleado: id_empleado[0],
+    nombres: nombres.value,
+    primer_apellido: primer_apellido.value,
+    segundo_apellido: segundo_apellido.value,
+    email: email.value,
+    fecha_nacimiento: fecha_nacimiento.value,
+    telefono: telefono.value,
+    codigo_postal: postcode.value,
+    pais: pais.value,
+    genero: genero.value,
+  });
+  setTimeout(() => {
+    window.location.reload();
+  }, 2000);
+  return response.data;
+};
 </script>
 
 <style scoped>
 .btn-status {
   border: none;
+}
+
+.btn {
+  box-shadow: none;
 }
 </style>
